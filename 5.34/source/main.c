337c57766b06180112aaa5aebd43b721e8f860ee
min4604 <mina4604sh@gmail.com>
2021-11-13 13:07:29 +0800

    finish 5.34

--- 5.34/source/main.c
+++ 5.34/source/main.c
@@ -1,28 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int maximum(int x, int y, int z);
+int power(int base, int exponent);
 
 int main(void)
 {
-	int number1, number2, number3;
-	printf("Enter three integers:");
-	scanf_s("%d %d %d", &number1, &number2, &number3);
-	printf("Maximum is : %d\n", maximum(number1, number2, number3));
+	int exp;
+	int base;
+	printf("Enter three base and exponent:");
+	scanf_s("%d %d",&base,&exp);
+	printf("%d^%d=%d\n",base,exp,power(base,exp));
 	system("pause");
 	return 0;
 }
 
-int maximum(int x, int y, int z)
+int power(int base, int exponent)
 {
-	int maxx = x;
-	if (y > maxx)
-	{
-		maxx = y;
-	}
-	if (z > maxx)
-	{
-		maxx = z;
-	}
-	return maxx;
+	if (exponent == 0)
+		return 1;
+	else if (exponent == 1)
+		return base;
+	else
+		return base * power(base, exponent - 1);
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # finish 5.28
 # finish 5.29
 # up     LAB
+# finish 5.34
