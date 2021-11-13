e94d27c01b5e25c3bd8fd94a3af1c8985e3034a8
min4604 <mina4604sh@gmail.com>
2021-11-13 16:29:09 +0800

    ok

--- 5.35/source/main.c
+++ 5.35/source/main.c
@@ -1,15 +1,44 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
+
+unsigned long long int fibonacci(unsigned int n);
 
 int main(void)
 {
-	int x;
-	for (x = 1; x <= 10; x++)
-	{
-		printf("%.2f ", sqrt(x));
-	}
+	unsigned int n;
+	printf("計算第n個費氏數列 N:");
+	scanf_s("%u", &n);
+	printf("第%u個:%I64u\n", n, fibonacci(n));
+	printf("能計算之最大費氏數列N=");
+	printf("\n值為:%I64u",fibonacci(UINT_MAX));
+	
 	printf("\n");
 	system("pause");
 	return 0;
+}
+
+unsigned long long int fibonacci(unsigned int n)
+{
+	unsigned long long int d1 = 0;
+	unsigned long long int d2 = 1;
+	unsigned long long int ram;
+	
+	for (unsigned int i = 2; i < n; i++)
+	{
+		ram = d1 + d2;
+		d1 = d2;
+		d2 = ram;
+		
+		if (d1 > d2)
+		{
+			printf("%u", i);
+			break;
+		}
+	}
+	if (n == 1)
+		return d1;
+	else if (n == 2)
+		return d2;
+	else
+		return ram;
 }
--- 5.36/source/main.c
+++ 5.36/source/main.c
@@ -1,19 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 
+void Hanoi(int n, int first, int end, int save);
 int main(void)
 {
-	char string1[60] = "Welcome";
-	char string2[60];
-
-	int len;
-	
-	strcpy_s(string2, 60, string1);
-	printf("string2=%s\n", string2);
-
-	len = strlen(string2);
-	printf("字串長度為%d\n", len);
+	int n, first, end, save;
+	printf("請輸入 圓盤個數 初始位置(1 or 2 or 3) 結束位置(1 or 2 or 3) 暫存區位置(1 or 2 or 3) : ");
+	scanf_s("%d %d %d %d", &n, &first, &end, &save);
+	Hanoi(n, first, end, save);
 	system("pause");
 	return 0;
+}
+
+void Hanoi(int n, int first, int end, int save)
+{
+	
+	if (n == 1)
+	{
+		printf("%d ---> %d\n", first, end);
+		return;
+	}
+	Hanoi(n - 1, first, save, end);
+	printf("%d ---> %d\n", first, end);
+	Hanoi(n - 1, save, end,first );
 }
